3a99cb9f168cf62ba13cc641c0bd281072737344
Ricardo <rr155914@gmail.com>
2021-07-02 14:25:56 -0500

    check linters

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let overlay = document.getElementById("myNav");
-let hamb = document.getElementById("Hamb");
-let closeNav = document.getElementsByClassName("closebtn")[0];
+const overlay = document.getElementById('myNav');
+const hamb = document.getElementById('Hamb');
+const closeNav = document.getElementsByClassName('closebtn')[0];
 
-// When the user clicks the button, open the modal 
+
 hamb.onclick = function() {
-    overlay.style.display = "block";
+    overlay.style.display = 'block';
   }
   
   // When the user clicks on <span> (x), close the modal
   closeNav.onclick = function() {
-    overlay.style.display = "none";
+    overlay.style.display = 'none';
   }
   
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == overlay) {
-      overlay.style.display = "none";
+      overlay.style.display = 'none';
     }
   }
 
